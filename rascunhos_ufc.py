30952960d8d06fd9a39baf46550231e78e220eaf
Raphael Levy <raphael.levy147@gmail.com>
2020-12-05 18:46:29 -0300

    Adiciona modelos

--- rascunhos_ufc.py
+++ rascunhos_ufc.py
@@ -39,51 +39,51 @@ print(data2.groupby("weight_class").mean())
 print(data2.groupby("gender").mean())
 print(data2.groupby(["weight_class", "gender"]).mean())
 
-vencedores_por_peso_genero = data2.groupby(["weight_class", "gender"]).mean()
-print(type(vencedores_por_peso_genero))
+rounds_por_peso_genero = data2.groupby(["weight_class", "gender"]).mean()
+print(type(rounds_por_peso_genero))
 
-vencedores_por_peso_genero["Winner"].plot.bar()
+rounds_por_peso_genero["no_of_rounds"].plot.bar()
 plt.show()
 
 
-df["VENCEDOR"] = data2.Winner
+df["NUMERO DE ROUNDS"] = data2.no_of_rounds
 print(df.head(3))
 
 print(df.describe())
 
-# #######################################################
+#######################################################
 
-# independentes = df.drop("VENCEDOR", axis = 1)
-# dependente = df["VENCEDOR"]
+independentes = df.drop("NUMERO DE ROUNDS", axis = 1)
+dependente = df["NUMERO DE ROUNDS"]
 
-# #Dividir o conjunto de dados entre treino e teste
+#Dividir o conjunto de dados entre treino e teste
 
-# X_treino, X_teste, Y_treino, Y_teste = model_selection.train_test_split(independentes, dependente, test_size = 0.4, random_state = 1)
-# print(X_treino.shape)
-# print(X_teste.shape)
-# print(Y_treino.shape)
-# print(Y_teste.shape)
+X_treino, X_teste, Y_treino, Y_teste = model_selection.train_test_split(independentes, dependente, test_size = 0.4, random_state = 1)
+print(X_treino.shape)
+print(X_teste.shape)
+print(Y_treino.shape)
+print(Y_teste.shape)
 
-# model = linear_model.LinearRegression()
-# model.fit(X_treino, Y_treino)
+model = linear_model.LinearRegression()
+model.fit(X_treino, Y_treino)
 
-# Y_previsto = model.predict(X_teste)
+Y_previsto = model.predict(X_teste)
 
-# plt.scatter(Y_teste, Y_previsto)
-# plt.xlabel("Vitórias")
-# plt.ylabel("Vitórias Previstas")
-# plt.title("Vitórias x Vitórias Previstas")
-# plt.show()
+plt.scatter(Y_teste, Y_previsto)
+plt.xlabel("Rounds")
+plt.ylabel("Rounds Previstos")
+plt.title("Rounds x Rounds Previstos")
+plt.show()
 
-# plt.scatter(model.predict(X_treino), model.predict(X_treino) - Y_treino, color = "green", s =10, label = "Treino")
-# plt.scatter(model.predict(X_teste), model.predict(X_teste) - Y_teste, color = "red", s =10, label = "Teste")
-# plt.show()
+plt.scatter(model.predict(X_treino), model.predict(X_treino) - Y_treino, color = "green", s =10, label = "Treino")
+plt.scatter(model.predict(X_teste), model.predict(X_teste) - Y_teste, color = "red", s =10, label = "Teste")
+plt.show()
 
-# print("\n------------------------------------------------\n")
-# print(model.score(X_treino, Y_treino))
-# print("Intercept:", model.intercept_)
-# print("Coef:", model.coef_)
+print("\n------------------------------------------------\n")
+print(model.score(X_treino, Y_treino))
+print("Intercept:", model.intercept_)
+print("Coef:", model.coef_)
 
-# print("\n------------------------------------------------\n")
-# mse = metrics.mean_squared_error(Y_teste, model.predict(X_teste))
-# print(mse)
+print("\n------------------------------------------------\n")
+mse = metrics.mean_squared_error(Y_teste, model.predict(X_teste))
+print(mse)
