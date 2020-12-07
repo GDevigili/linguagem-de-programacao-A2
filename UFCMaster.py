cd7a9befa680397ef3aa60f0a020abb118e370c1
João Lucas Duim <61877928+JLD819@users.noreply.github.com>
2020-12-07 10:36:11 -0300

    Adiciona documentação

--- UFCMaster.py
+++ UFCMaster.py
@@ -4,27 +4,28 @@ import pandas as pd
 #FAZER CLASSE AQUI
 
 class UFCMaster:
-    # Cada função é referente à uma pergunta
+    """Constroi um método para responder cada pergunta feita."""
     
     def __init__(self):
+        """Inicia a conexao com o banco de dados, importa os dados e encerra a conexao."""
         self.conexao = con.Conexao()
         self.df = self.conexao.getUfcDataFrame()
         self.conexao.fecharConexao()    
     
-    # Qual lado ganhou mais?
     def vitoriasPorLado(self):
+        """Responde a pergunta 'Qual lado ganhou mais?'"""
         vitoriasAzul = self.df[self.df["Winner"] == "Blue"]["Winner"].value_counts().head(1)[0]
         vitoriasVermelhas = self.df[self.df["Winner"] == "Red"]["Winner"].value_counts().head(1)[0]
         
-        if vitoriasVermelhas > vitoriasAzul:
+        if (vitoriasVermelhas > vitoriasAzul):
             return "Vermelho"
-        elif vitoriasAzul > vitoriasVermelhas:
+        elif (vitoriasAzul > vitoriasVermelhas):
             return "Azul"
         else:
             return "Empate"
         
-    # Quem ganhou mais lutas?
     def vitoriasPorLutador(self):
+        """Responde a pergunta 'Quem ganhou mais lutas?'"""
         vermelhos = self.df[self.df["Winner"] == "Red"]["R_fighter"]
         
         azuis = self.df[self.df["Winner"] == "Blue"]["B_fighter"]
@@ -32,8 +33,8 @@ class UFCMaster:
         
         return winners.value_counts()[:1].index.tolist()[0]
      
-    # Qual peso que tem mais integrantes?
     def contagemCategoria(self):
+        """Responde a pergunta 'Qual peso que tem mais integrantes?'"""
         
         grouped = self.df[['weight_class']].groupby(self.df['weight_class'])
         contGrupos = grouped.count()
@@ -42,13 +43,13 @@ class UFCMaster:
         print("A categoria de peso que mais tem integrantes é "+categoria)
         return categoria
         
-    # Quem ganhou mais vezes seguidas?
     def vitoriasSeguidas(self):
+        """Responde a pergunta 'Quem ganhou mais vezes seguidas?'"""
         maximoVitoriasSeguidasB = max(self.df["B_longest_win_streak"])
         maximoVitoriasSeguidasR = max(self.df["R_longest_win_streak"])
         
         
-        if(maximoVitoriasSeguidasB > maximoVitoriasSeguidasR):
+        if (maximoVitoriasSeguidasB > maximoVitoriasSeguidasR):
             return self.df[self.df["B_longest_win_streak"] == maximoVitoriasSeguidasB]["B_fighter"].values
         elif (maximoVitoriasSeguidasR > maximoVitoriasSeguidasB):
             return self.df[self.df["R_longest_win_streak"] == maximoVitoriasSeguidasR]["R_fighter"].values
@@ -58,13 +59,13 @@ class UFCMaster:
             losers = pd.concat([r,b], ignore_index=True)
             return losers.value_counts()[:1].index.tolist()[0]
     
-    # Quem perdeu mais vezes seguidas?
     def derrotasSeguidas(self):
+        """Responde a pergunta 'Quem perdeu mais vezes seguidas?'"""
         maximoDerrotasSeguidasB = max(self.df["B_current_lose_streak"])
         maximoDerrotasSeguidasR = max(self.df["R_current_lose_streak"])
         
         
-        if(maximoDerrotasSeguidasB > maximoDerrotasSeguidasR):
+        if (maximoDerrotasSeguidasB > maximoDerrotasSeguidasR):
             return self.df[self.df["B_current_lose_streak"] == maximoDerrotasSeguidasB]["B_fighter"].values
         elif (maximoDerrotasSeguidasR > maximoDerrotasSeguidasB):
             return self.df[self.df["R_current_lose_streak"] == maximoDerrotasSeguidasR]["R_fighter"].values
@@ -74,13 +75,12 @@ class UFCMaster:
             losers = pd.concat([r,b], ignore_index=True)
             return losers.value_counts()[:1].index.tolist()[0]
         
-    # Quantos rounds durou a maior luta?    
     def maiorLuta(self):
+        """Responde a pergunta 'Quantos rounds durou a maior luta?'"""
         return max(self.df["no_of_rounds"])
         
-    
-    # Quais partidas duraram menos?
     def menorLuta(self):
+        """Responde a pergunta 'Quais partidas duraram menos?'"""
         minimo = min(self.df["no_of_rounds"])
         return len(self.df[self.df["no_of_rounds"] == minimo])
     
