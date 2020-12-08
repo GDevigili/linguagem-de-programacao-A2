ac480aa64a89b14e304491eeb71094ffe42ba4ec
João Lucas Duim <jlduim@gmail.com>
2020-12-08 11:15:26 -0300

    Adiciona Testes

--- Test_CovidAeroporto.py
+++ Test_CovidAeroporto.py
@@ -1,21 +1,24 @@
 import CovidAeroporto as cov
 import unittest
 
-# Acesso à classe do arquivo CovidAeroporto não está funcionando
-# Arrumar respostas dos testes
+c = cov.CovidAeroporto()
 
 class TesteModuloCovidAeroporto(unittest.TestCase):
     def test_baselinesPorPais(self):
-        self.assertEqual(cov.baselinesPorPais(), DataFrame...)
+        self.assertEqual(c.baselinesPorPais(), DataFrame...)
         
     def test_baselinePorCidade(self):
-        self.assertEqual(cov.baselinePorCidade(), DataFrame...)
+        self.assertEqual(c.baselinePorCidade(), DataFrame...)
         
     def test_maiorNumeroVoosPorDia(self):
-        self.assertEqual(cov.maiorNumeroVoosPorDia(), DataFrame...)
+        self.assertEqual(c.maiorNumeroVoosPorDia(), DataFrame...)
     
     def test_menorNumeroVoosPorDia(self):
-        self.assertEqual(cov.menorNumeroVoosPorDia(), DataFrame...)
+        self.assertEqual(c.menorNumeroVoosPorDia(), DataFrame...)
         
     def test_baselinePorDia(self):
-        self.assertEqual(cov.baselinePorDia(), DataFrame...)
+        self.assertEqual(c.baselinePorDia(), DataFrame...)
+        
+if __name__ == '__main__':
+    # begin the unittest.main()
+    unittest.main()
