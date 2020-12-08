ca7dafcf284c1418bb2f88916554adc0f44b1bd2
Raphael Levy <raphael.levy147@gmail.com>
2020-12-08 11:23:26 -0300

    Adiciona colunas faltantes

--- Conexao.py
+++ Conexao.py
@@ -74,7 +74,8 @@ class Conexao:
                                     R_current_win_streak,
                                     country,
                                     B_total_rounds_fought,
-                                    R_total_rounds_fought
+                                    R_total_rounds_fought,
+                                    gender
                                FROM ufc.ufc_master;""", self.conexao)
         except pyodbc.Error as ex:
             sqlstate = ex.args[0]
