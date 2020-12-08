8fbf13c66f94e1f7e0e02bec923c1006a49880e9
JulianaCarvalhoSouza <jujucarvalho.souza@yahoo.com.br>
2020-12-08 11:42:08 -0300

    Apaga rascunhos

--- rascunhos.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn import datasets, linear_model, metrics, model_selection
-
-data1 = pd.read_csv("datasets/covid_impact_on_airport_traffic.csv", index_col=0)
-data2 = pd.read_csv("datasets/ufc-master.csv", index_col=0)
-
-#
