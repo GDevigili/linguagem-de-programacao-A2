0100505176b22325c6df6ca37030b16f4a762aea
Raphael Levy <raphael.levy147@gmail.com>
2020-12-08 12:35:35 -0300

    Corrige testes

--- TestCovidAeroporto.py
+++ TestCovidAeroporto.py
@@ -4,11 +4,6 @@ import unittest
 c = cov.CovidAeroporto()
 
 class TesteModuloCovidAeroporto(unittest.TestCase):
-    def test_baselinesPorPais(self):
-        self.assertEqual(c.baselinesPorPais(), DataFrame...)
-        
-    def test_baselinePorCidade(self):
-        self.assertEqual(c.baselinePorCidade(), DataFrame...)
         
     def test_maiorNumeroVoosPorDia(self):
         self.assertEqual(c.maiorNumeroVoosPorDia(), DataFrame...)
@@ -16,8 +11,26 @@ class TesteModuloCovidAeroporto(unittest.TestCase):
     def test_menorNumeroVoosPorDia(self):
         self.assertEqual(c.menorNumeroVoosPorDia(), DataFrame...)
         
-    def test_baselinePorDia(self):
-        self.assertEqual(c.baselinePorDia(), DataFrame...)
+    def test_maiorBaselinePorDia(self):
+        self.assertEqual(c.maiorBaselinePorDia(), DataFrame...)
+     
+    def test_menorBaselinePorCidade(self):
+        self.assertEqual(c.menorBaselinePorCidade(), DataFrame...)     
+     
+    def test_maiorBaselinePorEstado(self):
+        self.assertEqual(c.maiorBaselinePorEstado(), DataFrame...)     
+     
+    def test_maiorBaselinePorPais(self):
+        self.assertEqual(c.maiorBaselinePorPais(), DataFrame...)     
+        
+    def test_menorBaselinePorCidade(self):
+        self.assertEqual(c.menorBaselinePorCidade(), DataFrame...)    
+        
+    def test_menorBaselinePorEstado(self):
+        self.assertEqual(c.menorBaselinePorEstado(), DataFrame...)
+        
+    def test_menorBaselinePorPais(self):
+        self.assertEqual(c.menorBaselinePorPais(), DataFrame...)    
         
 if __name__ == '__main__':
     # begin the unittest.main()
